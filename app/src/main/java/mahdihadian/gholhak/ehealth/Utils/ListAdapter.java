da1660ad5ae11bf71cc257011cc97177c557af34
mahdi hadian <mahdihadian@hotmail.com>
2019-05-14 10:31:20 +0430

    bodyTemp and heartRate charts added

--- app/src/main/java/mahdihadian/gholhak/ehealth/Utils/ListAdapter.java
+++ app/src/main/java/mahdihadian/gholhak/ehealth/Utils/ListAdapter.java
@@ -1,10 +1,13 @@
 package mahdihadian.gholhak.ehealth.Utils;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -45,12 +48,30 @@ public class ListAdapter extends BaseAdapter {
         TextView txt_status = convertView.findViewById(R.id.txt_status);
         TextView txt_heart_beat = convertView.findViewById(R.id.txt_heart_beat);
         TextView txt_blood_pressure = convertView.findViewById(R.id.txt_blood_pressure);
+        LinearLayout layout = convertView.findViewById(R.id.layout);
 
         txt_name.setText(userModels.get(position).getName());
-        txt_age.setText(String.valueOf(userModels.get(position).getAge())+" Years old");
+        txt_age.setText(userModels.get(position).getAge() + "");
         txt_status.setText(userModels.get(position).getStatus());
         txt_heart_beat.setText(String.valueOf(userModels.get(position).getHeartBeat()));
         txt_blood_pressure.setText(String.valueOf(userModels.get(position).getBloodPressure()));
+
+        //        TODO set flat colors (integer data types...)
+
+        switch (userModels.get(position).getStatus()) {
+            case "Warning":
+                layout.setBackgroundColor(Color.YELLOW);
+                break;
+            case "Risk":
+                layout.setBackgroundColor(Color.RED);
+                break;
+            case "Good":
+                layout.setBackgroundColor(Color.GREEN);
+                break;
+            case "Normal":
+                layout.setBackgroundColor(Color.WHITE);
+                break;
+        }
 
         return convertView;
 
